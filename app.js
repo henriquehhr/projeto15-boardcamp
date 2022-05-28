aa1917ed49f0ca2cf05f8eba2cdf4d6fd03f4990
Henrique Herbs Rosa <henriquehhr@gmail.com>
2022-05-28 18:02:01 -0300

    feature: added routers

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 import express, {json} from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+
+import categoriesRouter from "./src/routers/categoriesRouter.js";
+import gamesRouter from "./src/routers/gamesRouter.js";
+import customersRouter from "./src/routers/customersRouter.js";
+import rentalsRouter from "./src/routers/rentalsRouter.js";
 
 dotenv.config();
 
 const app = express();
 app.use(json());
+app.use(cors());
+app.use(categoriesRouter);
+app.use(gamesRouter);
+app.use(customersRouter);
+app.use(rentalsRouter);
 
 app.listen(process.env.PORT, () => console.log(`Server live at http://localhost/${process.env.PORT}`));
